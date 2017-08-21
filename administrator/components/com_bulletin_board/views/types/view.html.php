1deed11e907cc0a22bfefd65b70d3df8ef6cb16e
Дмитрий <zatulenko@gmail.com>
2017-08-22 00:58:43 +0300

    Добавлен файл стилей и дополнительные кнопки в файл вида типов объявлений

--- administrator/components/com_bulletin_board/views/types/view.html.php
+++ administrator/components/com_bulletin_board/views/types/view.html.php
@@ -6,18 +6,32 @@ class BulletinBoardViewTypes extends JViewLegacy {
 	public function display($tpl = null)
 	{
 		$this->addToolBar();
+		$this->setDocument();
 		// Display the view
 		parent::display($tpl);
 	}
 
 	protected function addToolBar(){
-		JToolbarHelper::title(JText::_('COM_BULLETIN_BOARD_MANAGER_TYPES'));
+		JToolbarHelper::title(JText::_('COM_BULLETIN_BOARD_MANAGER_TYPES'),'bulletin-board');
 		JToolbarHelper::addNew('type.add',JText::_('COM_BULLETIN_BOARD_MANAGER_TYPES_ADD'));
-		JToolbarHelper::editList('type.edit',JText::_('COM_BULLETIN_BOARD_MANAGER_TYPES_EDIT'));
+		JToolbarHelper::editList('types.edit',JText::_('COM_BULLETIN_BOARD_MANAGER_TYPES_EDIT'));
+		JToolbarHelper::deleteList('types.delite');
 		JToolbarHelper::divider();
 		JToolbarHelper::publish('types.publish','JTOOLBAR_PUBLISH',TRUE);
 		JToolbarHelper::unpublish('types.unpublish','JTOOLBAR_UNPUBLISH',TRUE);
+		JToolbarHelper::custom('type.create','bulletin-board','bulletin-board_hover','JTOOLBAR_PUBLISH', false);
 		JToolbarHelper::preferences('com_bulletin_board');
+
+		// echo JUri::root()."<br/>";
+		// echo JUri::base()."<br/>";
+		// echo JUri::current()."<br/>";
+		//print_r(JUri::getInstance()->getVar('view'));
+	}
+
+	protected function setDocument(){
+		$doc = JFactory::getDocument();
+		echo JUri::root(true)."/media/com_bulletin_board/css/style.css";
+		$doc->addStyleSheet(JUri::root(true)."/media/com_bulletin_board/css/style.css");
 	}
 }
 
